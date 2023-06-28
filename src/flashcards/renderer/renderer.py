b152aaf6667db8a7379f5f26ed857b2fbebb0301
Wolf <caffeinate@msn.com>
2023-06-27 20:38:54 -0400

    Make renderers not a module

--- src/flashcards/renderer/renderer.py
+++ src/flashcards/renderer/renderer.py
@@ -3,20 +3,40 @@ import io
 
 from pypdf import PdfWriter
 
-from src.flashcards.graphics import icons
 from src.converter import convertToPdf
-from src.flashcards.renderers.sizes import sizes
+from src.flashcards.flashcard import Flashcard
+from src.flashcards.graphics import icons
+from src.flashcards.renderer import templates
+from src.flashcards.renderer.sizes import sizes
+from src.flashcards.styles import styles
 
 
 class GenericRendererUsageError(NotImplementedError):
     pass
 
 
-class GenericRenderer:
-    STYLE = "generic"
+class Renderer:
+    """
+    A renderer for flashcards.
 
-    def __init__(self, flashcard):
+    Fills in a flashcard SVG with the appropriate information, and renders it to a PDF.
+
+    Attributes:
+        flashcard: The flashcard to render.
+    """
+
+    def __init__(self, flashcard: Flashcard):
+        """
+        Initialize a GenericRenderer.
+        """
         self.flashcard = flashcard
+
+    @property
+    def style(self):
+        """
+        The style of the flashcard.
+        """
+        return self.flashcard.style
 
     def render(self, *args, **kwargs) -> bytes:
         """
@@ -43,7 +63,7 @@ class GenericRenderer:
         bytes_stream.seek(0)
         return bytes_stream.read()
 
-    def renderFront(self, size: str) -> bytes:
+    def renderFront(self, size: str) -> str:
         """
         Render the front of a flashcard to a base-64 PDF.
 
@@ -53,9 +73,9 @@ class GenericRenderer:
         Returns:
             str: The base-64 PDF.
         """
-        raise GenericRendererUsageError()
+        return self.renderBack(size)
 
-    def renderBack(self, size: str) -> bytes:
+    def renderBack(self, size: str) -> str:
         """
         Render the back of a flashcard to a base-64 PDF.
 
@@ -65,7 +85,38 @@ class GenericRenderer:
         Returns:
             str: The base-64 PDF.
         """
-        raise GenericRendererUsageError()
+        sentences = self._boldedSentences()
+
+        # fmt: off
+        fields = {}
+        for fieldName, fieldCount in styles[self.style].fields:
+            fieldName = fieldName.upper()
+            if fieldName > 1:
+                for i in range(1, fieldCount + 1):
+                    fields[f"{fieldName}_{i}"] = getattr(self.flashcard.fields, fieldName)[i]
+            else:
+                fields[fieldName] = getattr(self.flashcard.fields, fieldName)
+        # fmt: on
+
+        render = templates[self.style].renderFront(
+            WORD=self.flashcard.word.title(),
+            IMAGE=self.flashcard.fields.images[0],
+            PART_OF_SPEECH=self.flashcard.fields.partOfSpeech,
+            PART_OF_SPEECH_ICON=self._partOfSpeechIconBase64(),
+            PRONUNCIATION=self.flashcard.fields.pronunciation,
+            INSPIRATIONAL_QUOTE=self.flashcard.fields.inspirationalQuotes[0],
+            SYNONYM_1=self.flashcard.fields.synonyms[0],
+            SYNONYM_2=self.flashcard.fields.synonyms[1],
+            SYNONYM_3=self.flashcard.fields.synonyms[2],
+            SYNONYM_4=self.flashcard.fields.synonyms[3],
+            SYNONYM_5=self.flashcard.fields.synonyms[4],
+            SYNONYM_6=self.flashcard.fields.synonyms[5],
+            SENTENCE_1=sentences[0],
+            SENTENCE_2=sentences[1],
+            SENTENCE_3=sentences[2],
+        )
+
+        return self._processRender(render, size)
 
     def _processRender(self, render: str, size: str) -> str:
         """
@@ -111,6 +162,4 @@ class GenericRenderer:
         Returns:
             str: The base-64 encoded part of speech icon.
         """
-        return (
-            f"data:image/svg+xml;base64,{icons[self.flashcard.fields.partOfSpeech]}"
-        )
+        return f"data:image/svg+xml;base64,{icons[self.flashcard.fields.partOfSpeech]}"
