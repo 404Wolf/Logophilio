8cc81251834314ebf9a4d46453326e97e1ce22da
Wolf <caffeinate@msn.com>
2023-06-29 00:37:36 -0400

    Add Image class for flashcard images
    Allow for the storing of Image metadata such as prompts

---
+++ src/flashcards/utils/structs.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+
+@dataclass(slots=True, frozen=True)
+class Image:
+    """
+    Image dataclass for storing image data.
+
+    Attributes:
+        base64 (str): Base64 encoded image.
+        size (tuple[int, int]): Size of the image.
+        prompt (str): Prompt for the image.
+        dalleTemplate (str): Dalle template that the prompt was slotted into. This should
+            include a {prompt} placeholder.
+    """
+
+    base64: str = field(repr=False)
+    size: tuple[int, int]
+    prompt: str
+    dalleTemplate: str = field(repr=False)
