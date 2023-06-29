e512253a1189df35d7bf86a58b14729e6f5682f4
Wolf <caffeinate@msn.com>
2023-06-29 02:20:29 -0400

    Add Icon class

--- src/flashcards/graphics/icons.py
+++ src/flashcards/graphics/icons.py
@@ -1,11 +1,27 @@
 import json
+from dataclasses import dataclass
 
 from src.flashcards.utils.misc import fileAsBase64
 
 __all__ = ("icons",)
 
-with open("flashcards/graphics/icons/icons.json") as f:
+
+@dataclass(slots=True, frozen=True)
+class Icon:
+    name: str
+    path: str
+    base64: str
+
+    def __str__(self):
+        return f"data:image/svg+xml;base64,{self.base64}"
+
+
+with open ("flashcards/graphics/icons/icons.json") as f:
     icons = {
-        style: fileAsBase64(f"flashcards/graphics/icons/{style}.svg")
-        for style in json.load(f)
+        icon: Icon(
+            name=icon,
+            path=f"flashcards/graphics/icons/{icon}.svg",
+            base64=fileAsBase64(f"flashcards/graphics/icons/{icon}.svg"),
+        )
+        for icon in json.load(f)
     }
